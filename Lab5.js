030327cadd97eca9cab81dcf4ee1715d423673e5
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2023-11-16 11:05:26 -0500

    Completed up to 3.3.7

--- Lab5.js
+++ Lab5.js
@@ -1,4 +1,110 @@
+const assignment = {
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0
+};
+
+const todos = [
+    {id: 1, title: "Task 1", completed: false, description: "Description 1"},
+    {id: 2, title: "Task 2", completed: false, description: "Description 2"},
+    {id: 3, title: "Task 3", completed: false, description: "Description 3"},
+    {id: 4, title: "Task 4", completed: false, description: "Description 4"}
+]
+
 const Lab5 = (app) => {
+    // 3.2.4 Extra Credit
+    app.get("/a5/assignment/score/:newScore", (req, res) => {
+        const {newScore} = req.params;
+        assignment.score = newScore;
+        res.json(assignment);
+    })
+
+    app.get("/a5/assignment/completed/:newCompleted", (req, res) => {
+        const {newCompleted} = req.params;
+        assignment.completed = newCompleted;
+        res.json(assignment);
+    })
+
+    // 3.3.7 Extra Credit
+    app.get("/a5/todos/:id/completed/:completed", (req, res) => {
+        const {id, completed} = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.completed = JSON.parse(completed);
+        res.json(todos);
+    })
+
+    app.get("/a5/todos/:id/description/:description", (req, res) => {
+        const {id, description} = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.description = description;
+        res.json(todos)
+    })
+
+
+
+    app.get("/a5/assignment/completed", (req, res) => {
+        res.json(assignment.completed);
+    })
+
+    app.get("/a5/todos", (req, res) => {
+        const {completed} = req.query;
+        if (completed !== undefined) {
+            const completedTodos = todos.filter(
+                (t) => t.completed === completed
+            );
+            res.json(completedTodos);
+            return
+        }
+        res.json(todos)
+    })
+
+    app.get("/a5/todos/create", (req, res) => {
+        const newTodo = {
+            id: new Date().getTime(),
+            title: "New Task",
+            completed: false
+        };
+        todos.push(newTodo);
+        res.json(todos);
+    });
+
+    app.get("/a5/todos/:id/delete", (req, res) => {
+        const {id} = req.params;
+        const todo = todos.find((t) => t.id ===parseInt(id));
+        todos.splice(todos.indexOf(todo), 1);
+        res.json(todos);
+    })
+
+    app.get("/a5/todos/:id/title/:title", (req, res) => {
+        const {id, title} = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.title = title;
+        res.json(todos);
+    });
+
+    app.get("/a5/todos/:id", (req, res) => {
+        const {id} = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        res.json(todo);
+    })
+
+    app.get("/a5/assignment", (req, res) => {
+        res.json(assignment)
+    });
+
+    app.get("/a5/assignment/title/:newTitle", (req, res) => {
+        const {newTitle} = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    })
+
+    app.get('/a5/assignment/title', (req, res) => {
+        res.json(assignment.title);
+    })
+
     app.get("/a5/welcome", (req, res) => {
         res.send("Welcome to Assignment 5");
     });
@@ -14,5 +120,24 @@ const Lab5 = (app) => {
         const sum = parseInt(a) - parseInt(b);
         res.send(sum.toString());
     });
+
+    app.get("/a5/calculator", (req, res) => {
+        const {a, b, operation} = req.query;
+        let result = 0;
+        switch (operation) {
+            case "add":
+                result = parseInt(a) + parseInt(b);
+                break;
+
+            case "subtract":
+                result = parseInt(a) - parseInt(b);
+                break;
+
+            default:
+                result = "Invalid Operation"
+        }
+        res.send(result.toString());
+    });
+
 };
 export default Lab5;
